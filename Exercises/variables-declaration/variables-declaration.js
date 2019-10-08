31d5d2dfb1582c607ce5b0fefc9c64dce7914a59
Anna Dovzhyk <annd@ciklum.com>
2019-10-08 12:12:09 +0300

    vd

---
+++ Exercises/variables-declaration/variables-declaration.js
@@ -0,0 +1,18 @@
+//Declare a variable which will be encapsulated from global scope
+function Declare(){
+    let newElement = "name";
+    return newElement;
+}
+
+//Declare a variable which will appear in global object
+
+var user = "Name";
+
+function userName(user){
+
+}
+return userName();
+
+//Declare two variables which will be same-names but contain different values
+var user = "Surname";
+return userName();
